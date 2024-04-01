35c915ef90acbc2f2a9e3f42af08ae4e318c1000
himanushi <himanushi@users.noreply.github.com>
2024-04-02 08:32:28 +0900

    https://tokio.rs/tokio/tutorial/hello-tokio

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
-use reqwest;
+use mini_redis::{client, Result};
 
 #[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let body = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
+async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address.
+    let mut client = client::connect("127.0.0.1:6379").await?;
 
-    println!("{body}");
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
     Ok(())
 }
