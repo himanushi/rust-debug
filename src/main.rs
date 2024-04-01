a9ebd7b73785bb7af0af78e3f4fe24376f7c8b6c
himanushi <himanushi@users.noreply.github.com>
2024-04-01 15:03:43 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,12 @@
-use tokio::time::{sleep, Duration};
+use reqwest;
 
 #[tokio::main]
-async fn main() {
-    sleep(Duration::from_secs(10)).await;
-    println!("Hello, world!");
-}
+async fn main() -> Result<(), reqwest::Error> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
-    #[should_panic]
-    fn it_fails() {
-        assert_eq!(1 + 1, 3);
-    }
-
-    #[test]
-    #[ignore]
-    fn expensive_test() {
-        // 長い時間がかかるテスト
-    }
+    println!("body = {body:?}");
+    Ok(())
 }
