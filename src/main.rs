73797598340d8c4a42b099ec8f880e3f054bdb89
himanushi <himanushi@users.noreply.github.com>
2024-04-03 08:43:03 +0900

    https://tokio.rs/tokio/tutorial/spawning

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,17 @@
-use mini_redis::{Connection, Frame};
-use tokio::net::{TcpListener, TcpStream};
+use std::rc::Rc;
+use tokio::task::yield_now;
 
 #[tokio::main]
 async fn main() {
-    // Bind the listener to the address
-    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    tokio::spawn(async {
+        // The scope forces `rc` to drop before `.await`.
+        {
+            let rc = Rc::new("hello");
+            println!("{}", rc);
+        }
 
-    loop {
-        // The second item contains the IP and port of the new connection.
-        let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
-    }
-}
-
-async fn process(socket: TcpStream) {
-    // The `Connection` lets us read/write redis **frames** instead of
-    // byte streams. The `Connection` type is defined by mini-redis.
-    let mut connection = Connection::new(socket);
-
-    if let Some(frame) = connection.read_frame().await.unwrap() {
-        println!("GOT: {:?}", frame);
-
-        // Respond with an error
-        let response = Frame::Error("unimplemented".to_string());
-        connection.write_frame(&response).await.unwrap();
-    }
+        // `rc` is no longer used. It is **not** persisted when
+        // the task yields to the scheduler
+        yield_now().await;
+    });
 }
